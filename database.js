27c291f6a5d439fc724b21cc0aa34c0f9e81c311
mayado92 <115850057+mayado92@users.noreply.github.com>
2023-11-09 21:08:49 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,3 +2,14 @@ function search() {
     var outputObj = document.getElementById("output");
     outputObj.innerHTML 
 }
+function showAnswer(questionId) {
+    var answers = {
+        'question1': 'A: You can enter keywords in the search bar to find pictures.',
+        'question2': 'A: You can use the navigation buttons provided on top of the page.',
+        'question3': 'A: No, detailed descriptions are not provided, as we do not sell the displayed items .',
+        'question4': 'A: No, the download feature is not available. You can view and enjoy the pictures online though.'
+    };
+
+    var answer = answers[questionId];
+    alert(answer);
+}
